3847f14ed75527ca6be6c984de7f503c7cc8479f
choijinhap <wlsgkq123@gmail.com>
2020-06-24 10:44:20 +0900

    completing the game - declaring a winner

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ import { throwStatement } from '@babel/types';
             </button>
       );
   }
+
   class Board extends React.Component {
       constructor(props){
           super(props);
@@ -43,11 +44,14 @@ import { throwStatement } from '@babel/types';
       }
     handleClick(i){
           const squares=this.state.squares.slice();
+          if(calcuateWinner(squares)||squares[i]){
+              return;
+          }
           squares[i]=this.state.xIsNext?'X':'O';
           this.setState({
               squares:squares,
               xIsNext:!this.state.xIsNext,
-            })
+            });
       }
     renderSquare(i) {
       return <Square 
@@ -55,10 +59,15 @@ import { throwStatement } from '@babel/types';
       onClick={()=>this.handleClick(i)}
       />;
     }
-  
     render() {
-      const status = 'Next player: '+ (this.state.xIsNext?'X':'O');
-  
+        const winner=calcuateWinner(this.state.squares);
+        let status;
+        if(winner){
+            status='Winner :'+winner;
+        }else{
+            status='Next player: '+ (this.state.xIsNext?'X':'O');
+        }
+    
       return (
         <div>
           <div className="status">{status}</div>
@@ -97,7 +106,25 @@ import { throwStatement } from '@babel/types';
       );
     }
   }
-  
+  function calcuateWinner(squares){
+      const lines=[
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+          [0,4,8],
+          [2,4,6],
+      ];
+      for(let i=0;i<lines.length;i++){
+          const[a,b,c]=lines[i];
+          if(squares[a]&&squares[a]===squares[b]&&squares[a]===squares[c]){
+              return squares[a];
+          }
+      }
+      return null;
+  }
   // ========================================
   
   ReactDOM.render(
